a7abafdd8929683813544675bd7aa8092ef0908b
dextroxd <divyanshu171299@gmail.com>
2021-10-03 19:09:22 +0530

    File Uploading Service

---
+++ src/main/java/com/dextroxd/springbootfileuploader/SpringBootFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.dextroxd.springbootfileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dextroxd/springbootfileuploader/SpringBootFileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextroxd.springbootfileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
